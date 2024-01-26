9b0d73069e6d519af689a3595e7b725162bca839
Nolan Flammini <nolantheturtle@gmail.com>
2024-01-26 03:40:49 -0800

    [stable] drawing backbuffer with correct color, new BitsPerPixel calc

--- BitmapHeader.cs
+++ BitmapHeader.cs
@@ -9,7 +9,7 @@ namespace FoundationR
     //  DWORD           4 bytes uint
     //  LONG            4 bytes int
     //  WORD            2 bytes short
-    //  CIEXYZTRIPLE    ? bytes object 
+    //  CIEXYZTRIPLE    ? bytes object
     public struct CIEXYZTRIBPLE
     {
         public CIEXYZ[] TRIPLE;
@@ -18,7 +18,80 @@ namespace FoundationR
     {
         public int R, G, B;
     }
-    public struct tagBITMAPV3INFOHEADER
+    public struct BITMAPINFOHEADER
+    {
+        internal uint biSize;
+        internal int biWidth;
+        internal int biHeight;
+        internal short biPlanes;
+        internal short biBitCount;
+        internal uint biCompression;
+        internal uint biSizeImage;
+        internal int biXPelsPerMeter;
+        internal int biYPelsPerMeter;
+        internal uint biClrUsed;
+        internal uint biClrImportant;
+        public static readonly uint BitmapHeader = 14;
+        public static readonly uint HeaderOffset = 40;
+        public static byte[] CreateDIBHeader(BITMAPINFOHEADER header)
+        {
+            byte[] array =
+                //Size of DIB header
+                BitConverter.GetBytes(HeaderOffset)
+                //Image width
+                .Concat(BitConverter.GetBytes(header.biWidth))
+                //Image height
+                .Concat(BitConverter.GetBytes(header.biHeight))
+                //# of color planes being used
+                .Concat(BitConverter.GetBytes(header.biPlanes))
+                //Pixel format
+                .Concat(BitConverter.GetBytes(header.biBitCount))
+                //Compression, if raw, normally 0
+                .Concat(BitConverter.GetBytes(header.biCompression))
+                //Size of the pixel array (including padding)
+                .Concat(BitConverter.GetBytes(header.biSizeImage))
+                //Horizontal resolution of the image (96)
+                .Concat(BitConverter.GetBytes(header.biXPelsPerMeter))
+                //Vertical resolution of the image (96)
+                .Concat(BitConverter.GetBytes(header.biYPelsPerMeter))
+                //# of colors in the color palette
+                .Concat(BitConverter.GetBytes(header.biClrUsed))
+                //# of important colors used (0 means all)
+                .Concat(BitConverter.GetBytes(header.biClrImportant))
+                .ToArray();
+            return array;
+        }
+        public static byte[] CreateDIBHeader(REW image, out BITMAPINFOHEADER header)
+        {
+            header = new BITMAPINFOHEADER();
+            byte[] array = 
+                //Size of DIB header
+                BitConverter.GetBytes(HeaderOffset)
+                //Image width
+                .Concat(BitConverter.GetBytes(header.biWidth = (int)image.Width))
+                //Image height
+                .Concat(BitConverter.GetBytes(header.biHeight = (int)image.Height))
+                //# of color planes being used
+                .Concat(BitConverter.GetBytes(header.biPlanes = (short)0))
+                //Pixel format
+                .Concat(BitConverter.GetBytes(header.biBitCount = image.BitsPerPixel))
+                //Compression, if raw, normally 0
+                .Concat(BitConverter.GetBytes(header.biCompression = (uint)CompressionMethod.BI_RGB))
+                //Size of the pixel array (including padding)
+                .Concat(BitConverter.GetBytes(header.biSizeImage = (uint)image.RealLength))
+                //Horizontal resolution of the image (96)
+                .Concat(BitConverter.GetBytes(header.biXPelsPerMeter = 96))
+                //Vertical resolution of the image (96)
+                .Concat(BitConverter.GetBytes(header.biYPelsPerMeter = 96))
+                //# of colors in the color palette
+                .Concat(BitConverter.GetBytes(header.biClrUsed = 0))
+                //# of important colors used (0 means all)
+                .Concat(BitConverter.GetBytes(header.biClrImportant = 0))
+                .ToArray();
+            return array;
+        }
+    }
+    public struct BITMAPV3INFOHEADER
     {
         internal uint biSize;
         internal int biWidth;
@@ -45,30 +118,69 @@ namespace FoundationR
         internal uint biProfileSize;
         internal uint biReserved;
         public static readonly uint BitmapHeader = 14;
-        public static readonly uint HeaderOffset = 0;
-        public byte[] CreateHeader(REW rew)
+        public static readonly uint HeaderOffset = 120;
+        public static byte[] CreateDIBHeader(BITMAPV3INFOHEADER header)
         {
+            byte[] array = new byte[header.biSizeImage + HeaderOffset + BitmapHeader];
+            Array.Copy(BitConverter.GetBytes(header.biSize), 0, array, 0, 4);
+            Array.Copy(BitConverter.GetBytes(header.biWidth), 0, array, 4, 4);
+            Array.Copy(BitConverter.GetBytes(header.biHeight), 0, array, 8, 4);
+            Array.Copy(BitConverter.GetBytes(header.biPlanes), 0, array, 12, 2);
+            Array.Copy(BitConverter.GetBytes(header.biBitCount), 0, array, 14, 2);
+            Array.Copy(BitConverter.GetBytes(header.biCompression), 0, array, 16, 4);
+            Array.Copy(BitConverter.GetBytes(header.biSizeImage), 0, array, 20, 4);
+            Array.Copy(BitConverter.GetBytes(header.biXPelsPerMeter), 0, array, 24, 4);
+            Array.Copy(BitConverter.GetBytes(header.biYPelsPerMeter), 0, array, 28, 4);
+            Array.Copy(BitConverter.GetBytes(header.biClrUsed), 0, array, 32, 4);
+            Array.Copy(BitConverter.GetBytes(header.biClrImportant), 0, array, 36, 4);
+            Array.Copy(BitConverter.GetBytes(header.biRedMask), 0, array, 40, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGreenMask), 0, array, 44, 4);
+            Array.Copy(BitConverter.GetBytes(header.biBlueMask), 0, array, 48, 4);
+            Array.Copy(BitConverter.GetBytes(header.biAlphaMask ), 0, array, 52, 4);
+            Array.Copy(BitConverter.GetBytes(header.biCSType), 0, array, 56, 4);
+            Array.Copy(BitConverter.GetBytes(header.biAlphaMask), 0, array, 60, 4);
+            Array.Copy(BitConverter.GetBytes(header.biCSType), 0, array, 64, 4);
+            Array.Copy(BitConverter.GetBytes(/*ciexyzTriple*/24), 0, array, 68, 24);
+            Array.Copy(BitConverter.GetBytes(header.biGammaRed), 0, array, 92, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGammaGreen), 0, array, 96, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGammaBlue), 0, array, 100, 4);
+            Array.Copy(BitConverter.GetBytes(header.biIntent), 0, array, 104, 4);
+            Array.Copy(BitConverter.GetBytes(header.biProfileData), 0, array, 108, 4);
+            Array.Copy(BitConverter.GetBytes(header.biProfileSize), 0, array, 112, 4);
+            Array.Copy(BitConverter.GetBytes(header.biReserved), 0, array, 116, 4);
+            return array;
+        }
+        public static byte[] CreateDIBHeader(REW rew, out BITMAPV3INFOHEADER header)
+        {
+            header = new BITMAPV3INFOHEADER();
             byte[] array = new byte[rew.RealLength + HeaderOffset + BitmapHeader];
-            Array.Copy(BitConverter.GetBytes(biSize = HeaderOffset), 0, array, 0, 4);
-            Array.Copy(BitConverter.GetBytes(biWidth = (int)rew.Width), 0, array, 4, 4);
-            Array.Copy(BitConverter.GetBytes(biHeight = (int)rew.Height), 0, array, 8, 4);
-            Array.Copy(BitConverter.GetBytes(biPlanes = 0), 0, array, 12, 2);
-            Array.Copy(BitConverter.GetBytes(biBitCount = 32), 0, array, 14, 2);
-            Array.Copy(BitConverter.GetBytes(biCompression = (uint)CompressionMethod.BI_ALPHABITFIELDS), 0, array, 14, 4);
-            Array.Copy(BitConverter.GetBytes(biSizeImage = (uint)rew.RealLength), 0, array, 18, 4);
-            Array.Copy(BitConverter.GetBytes(biXPelsPerMeter = 96), 0, array, 22, 4);
-            Array.Copy(BitConverter.GetBytes(biYPelsPerMeter = 96), 0, array, 26, 4);
-            Array.Copy(BitConverter.GetBytes(biClrUsed = 0), 0, array, 30, 4);
-            Array.Copy(BitConverter.GetBytes(biClrImportant = 0), 0, array, 34, 4);
-            Array.Copy(BitConverter.GetBytes(biRedMask = 0), 0, array, 38, 4);
-            Array.Copy(BitConverter.GetBytes(biGreenMask =  0), 0, array, 42, 4);
-            Array.Copy(BitConverter.GetBytes(biBlueMask = 0), 0, array, 46, 4);
-            Array.Copy(BitConverter.GetBytes(biAlphaMask = 0), 0, array, 50, 4);
-            Array.Copy(BitConverter.GetBytes(biCSType = 0), 0, array, 54, 4);
-            Array.Copy(BitConverter.GetBytes(biAlphaMask = 0), 0, array, 58, 4);
-            Array.Copy(BitConverter.GetBytes(biCSType = 0), 0, array, 62, 4);
-            Array.Copy(BitConverter.GetBytes(/*ciexyzTriple*/18), 0, array, 66, 18);
-            Array.Copy(BitConverter.GetBytes(biGammaRed = 0), 0, array, 84, 4);
+            Array.Copy(BitConverter.GetBytes(header.biSize = HeaderOffset), 0, array, 0, 4);
+            Array.Copy(BitConverter.GetBytes(header.biWidth = (int)rew.Width), 0, array, 4, 4);
+            Array.Copy(BitConverter.GetBytes(header.biHeight = (int)rew.Height), 0, array, 8, 4);
+            Array.Copy(BitConverter.GetBytes(header.biPlanes = 0), 0, array, 12, 2);
+            Array.Copy(BitConverter.GetBytes(header.biBitCount = 32), 0, array, 14, 2);
+            Array.Copy(BitConverter.GetBytes(header.biCompression = (uint)CompressionMethod.BI_ALPHABITFIELDS), 0, array, 16, 4);
+            Array.Copy(BitConverter.GetBytes(header.biSizeImage = (uint)rew.RealLength), 0, array, 20, 4);
+            Array.Copy(BitConverter.GetBytes(header.biXPelsPerMeter = 96), 0, array, 24, 4);
+            Array.Copy(BitConverter.GetBytes(header.biYPelsPerMeter = 96), 0, array, 28, 4);
+            Array.Copy(BitConverter.GetBytes(header.biClrUsed = 0), 0, array, 32, 4);
+            Array.Copy(BitConverter.GetBytes(header.biClrImportant = 0), 0, array, 36, 4);
+            Array.Copy(BitConverter.GetBytes(header.biRedMask = 0), 0, array, 40, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGreenMask =  0), 0, array, 44, 4);
+            Array.Copy(BitConverter.GetBytes(header.biBlueMask = 0), 0, array, 48, 4);
+            Array.Copy(BitConverter.GetBytes(header.biAlphaMask = 0), 0, array, 52, 4);
+            Array.Copy(BitConverter.GetBytes(header.biCSType = 0), 0, array, 56, 4);
+            Array.Copy(BitConverter.GetBytes(header.biAlphaMask = 0), 0, array, 60, 4);
+            Array.Copy(BitConverter.GetBytes(header.biCSType = 0), 0, array, 64, 4);
+            Array.Copy(BitConverter.GetBytes(/*ciexyzTriple*/24), 0, array, 68, 24);
+            Array.Copy(BitConverter.GetBytes(header.biGammaRed = 0), 0, array, 92, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGammaGreen = 0), 0, array, 96, 4);
+            Array.Copy(BitConverter.GetBytes(header.biGammaBlue = 0), 0, array, 100, 4);
+            Array.Copy(BitConverter.GetBytes(header.biIntent = 0), 0, array, 104, 4);
+            Array.Copy(BitConverter.GetBytes(header.biProfileData = 0), 0, array, 108, 4);
+            Array.Copy(BitConverter.GetBytes(header.biProfileSize = 0), 0, array, 112, 4);
+            Array.Copy(BitConverter.GetBytes(header.biReserved = 0), 0, array, 116, 4);
+            return array;
         }
     }
     //BITMAPV3INFOHEADER, * PBITMAPV3INFOHEADER;
